94515ef19f6fdc6eb19ab0ad7810b0b1895b6edc
BK <ma1223400461@outlook.com>
2020-06-15 14:31:54 +0800

    Add BaseReponseModel

---
+++ Inspirator.Model/DTO/Enum/StatusCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspirator.Model.DTO.Enum
+{
+    public enum StatusCode
+    {
+        #region Sucess
+        Sucess = 1000,
+        #endregion
+        #region Erroe
+        /// <summary>
+        /// 未知错误
+        /// </summary>
+        UnkowError = 2001,
+        /// <summary>
+        /// 无权限
+        /// </summary>
+        NoPermission = 2004,
+        /// <summary>
+        /// 认证失败
+        /// </summary>
+        AuthenticationFailed = 2005,
+        /// <summary>
+        /// 资源不存在
+        /// </summary>
+        NotFound = 2007,
+        /// <summary>
+        /// 参数错误
+        /// </summary>
+        ParameterErroe = 2006,
+        /// <summary>
+        /// 令牌失效
+        /// </summary>
+        TokenInvalidation = 2006
+        #endregion
+    }
+}
---
+++ Inspirator.Model/DTO/UnifyResponseDto.cs
@@ -0,0 +1,46 @@
+﻿using Inspirator.Model.DTO.Enum;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspirator.Model.DTO
+{
+    public class UnifyResponseDto
+    {
+        public StatusCode Code { get; set; }
+        public string Message { get; set; }
+
+        public UnifyResponseDto(StatusCode code, string message)
+        {
+            Code = code;
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+
+        public UnifyResponseDto()
+        {
+        }
+
+        public static UnifyResponseDto Sucess(string message = "操作成功")
+        {
+            return new UnifyResponseDto
+            {
+                Code = StatusCode.Sucess,
+                Message = message
+            };
+        }
+        public static UnifyResponseDto Sucess(StatusCode code = StatusCode.UnkowError,string message = "操作失败")
+        {
+            return new UnifyResponseDto
+            {
+                Code = code,
+                Message = message
+            };
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
