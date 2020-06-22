5be50e6eef9059339dfae86cafa5512c204dd8ae
BK <ma1223400461@outlook.com>
2020-06-23 00:36:16 +0800

    add sampleoptionrepositor

---
+++ Inspirator.Repository/ISampleOptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using Inspirator.IRepository;
+using Inspirator.Model.Context;
+using Inspirator.Model.Entities;
+
+namespace Inspirator.Repository
+{
+    public class SampleOptionRepository : GenericRepository<SampleOption>, ISampleOptionRepository
+    {
+        public SampleOptionRepository(MainContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Inspirator.Service/SampleOptionsService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Inspirator.IRepository;
+using Inspirator.IService;
+using System;
+
+namespace Inspirator.Service
+{
+    public class SampleOptionsService : ISampleOptionService
+    {
+        private readonly ISampleRepository _repository;
+        private readonly ISampleService _sampleService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SampleOptionsService(ISampleRepository repository, IUnitOfWork unitOfWork, IMapper mapper, ISampleService sampleService)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _sampleService = sampleService ?? throw new ArgumentNullException(nameof(sampleService));
+        }
+    }
+}
